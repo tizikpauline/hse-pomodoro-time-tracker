7d33c14d51cc528b9f82cc9c21e3c025a4a25df4
Tizik Pauline <43169531+tizikpauline@users.noreply.github.com>
2019-04-09 13:23:16 +0300

    поправила скрипт

--- script.js
+++ script.js
@@ -8,23 +8,43 @@ function startTimer(e) {
   e.preventDefault()
 
   const input = e.target.querySelector("input")
-
   input.disabled = true
+
+  const key = Date.now()
+  localStorage.setItem(key, input.value)
+
   progress.style.width = "100vw"
   progress.style.transitionDuration = WORK_TIME + "ms"
 
   setTimeout(function () {
     input.disabled = false
     input.focus()
-    input.value = " "
 
-    progress.style.transitionDuration = " "
-    progress.style.width = " "
+    progress.style.transitionDuration = ""
+    progress.style.width = ""
 
     const listItem = document.createElement("li")
     listItem.innerText = input.value
     tasks.appendChild(listItem)
+
+    input.value = ""
   }, WORK_TIME)
 }
 
+function loadHistory() {
+  const historySize = localStorage.length
+
+  if (historySize > 0) {
+    for (let i = 0; i < historySize; i++) {
+      const key = localStorage.key(i)
+      const taskName = localStorage.getItem(key)
+
+      const listItem = document.createElement("li")
+      listItem.innerText = taskName
+      tasks.appendChild(listItem)
+    }
+  }
+}
+
 form.onsubmit = startTimer
+loadHistory()
