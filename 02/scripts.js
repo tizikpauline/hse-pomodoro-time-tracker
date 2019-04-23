441729ffb772d1a7bd9c8013dc570a97b16d981a
Tizik Pauline <43169531+tizikpauline@users.noreply.github.com>
2019-04-23 12:35:58 +0300

    script

--- 02/scripts.js
+++ 02/scripts.js
@@ -33,24 +33,37 @@ statsLink.onclick = function () {
   timerLink.classList.remove('is-active')
 
   const historySize = localStorage.length
+
   if (historySize > 0) {
     for (let i = 0; i < historySize; i++) {
       const key = localStorage.key(i)
       const taskName = localStorage.getItem(key)
 
       const tr = document.createElement('tr')
+
+      const taskDate = new Date(+key).getDate()
+      const taskMonth = new Date(+key).getMonth()
       const tdDate = document.createElement('td')
+
+      // Как сделать из строки число:
+      //   1. Number(key)
+      //   2. parseInt(key)
+      //   3. +key
+
+      tdDate.innerText = taskDate + '/' + taskMonth
+
       const tdTime = document.createElement('td')
       const tdTask = document.createElement('td')
       tdTask.innerText = taskName
       const tdRemove = document.createElement('td')
+      // [tdDate, tdTime, tdTask, tdRemove]
+      //   .forEach(el => tr.appendChild(el))
       tr.appendChild(tdDate)
       tr.appendChild(tdTime)
       tr.appendChild(tdTask)
       tr.appendChild(tdRemove)
-      // const listItem = document.createElement('li')
-      // listItem.innerText = taskName
-      // tasks.appendChild(listItem)
+      const tbody = document.querySelector('tbody')
+      tbody.appendChild(tr)
     }
   }
 }
@@ -60,7 +73,13 @@ form.onsubmit = function startTimer(e) {
 
   const input = e.target.querySelector('input[type=text]')
   const key = Date.now()
-  localStorage.setItem(key, input.value)
+  // Создаем объект с названием и временем задачи
+  const task = {
+    name: input.value,
+    time: taskTime
+  }
+  // Сохраняем объект в память браузера, но предварительно переводим объект в строку — в памяти браузера можно хратить только строки
+  localStorage.setItem(key, JSON.stringify(task))
 
   setTimeout(function () {
     input.value = ''
